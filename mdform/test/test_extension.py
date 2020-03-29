e754d6f16d35d0d2b4d4daec9793b57adf4fc50c
Hernan <hernan.grecco@gmail.com>
2020-03-29 18:16:19 -0300

    Added test for wtf=True

--- mdform/test/test_extension.py
+++ mdform/test/test_extension.py
@@ -19,6 +19,12 @@ JINJA = """<p>Welcome to the form tester</p>
 {{ form.really_annoying_323_name }}</p>
 <p>{{ form.user_name }}</p>"""
 
+JINJA_WTF = """<p>Welcome to the form tester</p>
+<p>{{ wtf.form_field(form.name, form_type='horizontal', class="form-control", maxlength=30) }}
+{{ wtf.form_field(form.e_mail, form_type='horizontal', class="form-control") }}
+{{ wtf.form_field(form.really_annoying_323_name, form_type='horizontal', class="form-control") }}</p>
+<p>{{ wtf.form_field(form.user_name, form_type='horizontal', class="form-control") }}</p>"""
+
 FORM = {
     "name": dict(label="name", type="StringField", required=True, length=30),
     "e_mail": dict(label="e-mail", type="EmailField", required=True),
@@ -36,4 +42,10 @@ FORM = {
 def test_default():
     md = Markdown(extensions=[FormExtension()])
     assert md.convert(TEXT) == JINJA
+    assert md.Form == FORM
+
+
+def test_wtf():
+    md = Markdown(extensions=[FormExtension(wtf=True)])
+    assert md.convert(TEXT) == JINJA_WTF
     assert md.Form == FORM
